951e46f9596711ae95abad79bfb3670c87edee93
Christian <cholland2408@gmail.com>
2018-11-13 10:31:05 +0100

    Added bookmark buttons

--- server.R
+++ server.R
@@ -25,10 +25,15 @@ kinact_regulon_human = readRDS("data/kinact_regulon_human.rds")
   
 options(shiny.maxRequestSize=30*1024^2)
 
-server <- function(input, output, session) {
+server = function(input, output, session) {
+
+
   source("sub/02_server_upload.R", local=T)
   source("sub/03_server_dorothea.R", local=T)
   # source("sub/04_server_progeny.R", local=T)
   # source("sub/05_server_kinact.R", local=T)
   # source("sub/06_server_integration.R", local=T)
+  source("sub/server_bookmark.R", local=T)
+  
+
 }
