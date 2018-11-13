4b163edfd2652007a8f409114517c43d9083f66f
Christian <cholland2408@gmail.com>
2018-11-13 10:32:42 +0100

    minor changes

--- ui.R
+++ ui.R
@@ -1,22 +1,24 @@
 # UI
 source("sub/global.R")
 
-ui = fluidPage(
-  useShinyjs(),
-  tags$head(includeScript("google-analytics.js")),
-  #shinyjs::addClass(id = "welcome", class = "navbar-right"),
-  #theme = shinythemes::shinytheme("spacelab"),
-  navbarPage(
-    id = "menu", title="FUNKI",collapsible=T,
-    footer = column(12, align="center", "FUNKI-App 2018 (version: 0.1)"),
-    source("sub/01_ui_welcome.R")$value,
-    source("sub/02_ui_upload.R")$value,
-    source("sub/03_ui_dorothea.R")$value
-    #,source("sub/04_ui_progeny.R")$value
-    #,source("sub/05_ui_kinact.R")$value
-    #,source("sub/06_ui_integration.R")$value
-    ,source("sub/ui_help.R")$value
-    ,source("sub/ui_contact.R")$value
-    ,hr()
-    ) # close navbarPage
-  ) # close fluidPage
+ui = function(request) {
+  fluidPage(
+    useShinyjs(),
+    tags$head(includeScript("google-analytics.js")),
+    #shinyjs::addClass(id = "welcome", class = "navbar-right"),
+    #theme = shinythemes::shinytheme("spacelab"),
+    navbarPage(
+      id = "menu", title="FUNKI",collapsible=T,
+      footer = column(12, align="center", "FUNKI-App 2018 (version: 0.1)"),
+      source("sub/01_ui_welcome.R")$value,
+      source("sub/02_ui_upload.R")$value,
+      source("sub/03_ui_dorothea.R")$value
+      #,source("sub/04_ui_progeny.R")$value
+      #,source("sub/05_ui_kinact.R")$value
+      #,source("sub/06_ui_integration.R")$value
+      ,source("sub/ui_help.R")$value
+      ,source("sub/ui_contact.R")$value
+      ,hr()
+      ) # close navbarPage
+    ) # close fluidPage
+}
