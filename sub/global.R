9e01777d465d8dbb947d9b9da98ce6275cd84fd4
rosherbal <rosa.hernansaiz@bioquant.uni-heidelberg.de>
2020-07-16 14:42:36 +0200

    plots for visualization

--- sub/global.R
+++ sub/global.R
@@ -1,157 +1,73 @@
+
+#initialize
+
 library(shiny)
-library(shinyWidgets)
-library(tidyverse)
-library(viper)
-library(ggrepel)
 library(DT)
-library(furrr)
-library(pheatmap)
-library(shinyjs)
-library(ggExtra)
-library(tidygraph)
-library(ggraph)
-library(broom)
+library(tidyverse)
+library(dorothea)
+library(ggplot2)
+library(dplyr)
+library(reshape2)
 
 # shiny options
 enableBookmarking(store = "server")
 options(shiny.maxRequestSize=30*1024^2)
 
-# load data
-rwth_colors_df = get(load("data/misc/rwth_colors.rda"))
-example_data = read_csv("data/examples/example_data.csv") 
+# Load data
+dorothea_result = read.csv( "data/examples/dorothea_scores_ABC.csv", row.names = 1 )
+data(dorothea_hs, package = "dorothea")
 
-# dorothea
-load("data/models/dorothea_regulon_human_v1.rda")
-load("data/models/dorothea_regulon_mouse_v1.rda")
 
-load("data/models/dorothea_regulon_human_coverage_v1.rda")
-load("data/models/dorothea_regulon_mouse_coverage_v1.rda")
+# PLOTS -------------------------------------------------------------
 
-# progeny
-load("data/models/progeny_matrix_mouse_v1.rda")
-load("data/models/progeny_matrix_human_v1.rda")
-
-# kinact
-kinact_regulon_human = readRDS("data/models/kinact_regulon_human.rds")
-
-# functions
-run_progeny = function (E, M, gene_name = "gene", value_name = "expression",
-                        id_name = "sample", permutation = 10000, ...) {
-  plan(multiprocess)
-  E = E %>% mutate_if(is.factor, as.character)
+barplot_nes = function(df, smpl, nHits){
   
-  if (permutation > 0) {
-    null_model = future_map_dfr(1:permutation, .progress = T, function(p) {
-      E %>%
-        group_by(!!!syms(id_name)) %>%
-        sample_frac() %>%
-        ungroup() %>%
-        mutate(!!gene_name := E[[gene_name]]) %>%
-        run_progeny(M, gene_name = gene_name, value_name = value_name,
-                    id_name = id_name, permutation = 0)
-    }) %>%
-      group_by(!!!syms(id_name), pathway) %>%
-      summarise(m = mean(activity),
-                s = sd(activity)) %>%
-      ungroup()
-  }
+  df = df[, c("GeneID", smpl)] %>%
+    dplyr::rename(NES = smpl) %>%
+    dplyr::top_n(nHits, wt = abs(NES)) %>%
+    dplyr::arrange(NES) %>%
+    dplyr::mutate(GeneID = factor(GeneID))
+    
+  title = paste("Sample/Contrast:", smpl, sep = " ")
   
-  meta_data = E %>%
-    select(-c(!!gene_name, !!value_name)) %>%
-    distinct()
-  
-  emat = E %>%
-    select(!!gene_name, !!id_name, !!value_name) %>%
-    spread(!!id_name, !!value_name, fill = 0) %>%
-    drop_na() %>%
-    data.frame(row.names = 1, stringsAsFactors = F, check.names = F)
-  
-  model = M %>%
-    spread(pathway, weight, fill = 0) %>%
-    data.frame(row.names = 1, check.names = F, stringsAsFactors = F)
-  
-  common_genes = intersect(rownames(emat), rownames(model))
-  emat_matched = emat[common_genes, , drop = FALSE] %>%
-    t()
-  model_matched = model[common_genes, , drop = FALSE] %>%
-    data.matrix()
-  
-  stopifnot(names(emat_matched) == rownames(model_matched))
-  
-  progeny_scores = emat_matched %*% model_matched %>%
-    data.frame(stringsAsFactors = F, check.names = F) %>%
-    rownames_to_column(id_name) %>%
-    gather(key = pathway, value = activity, -!!id_name) %>%
-    as_tibble() %>%
-    inner_join(meta_data, by = id_name)
-  
-  if (permutation > 0) {
-    progeny_z_scores = progeny_scores %>%
-      inner_join(null_model, by = c(id_name, "pathway")) %>%
-      mutate(activity = (activity - m)/s) %>%
-      select(!!id_name, pathway, activity)
-    return(progeny_z_scores)
-  }
-  else {
-    return(progeny_scores)
-  }
+  ggplot(df, aes(x = reorder(GeneID, NES), y = NES)) +
+    geom_bar(aes(fill = NES), stat = "identity") +
+    scale_fill_gradient2(low = "darkblue", high = "indianred",
+                         mid = "whitesmoke", midpoint = 0) +
+    theme_minimal() +
+    theme(axis.title = element_text(face = "bold", size = 12),
+          axis.text.x =
+            element_text(angle = 45, hjust = 1, size =10, face= "bold"),
+          axis.text.y = element_text(size =10, face= "bold"),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank()) +
+    xlab("Transcription Factors") +
+    ylab("Normalized Enrichment scores (NES)") +
+    ggtitle(title)
 }
 
-run_viper = function(E, regulon, gene_name = "gene", value_name = "expression",
-                     id_name = "sample", regulator_name = "tf",  ...) {
-  meta_data = E %>%
-    select(-c(!!gene_name, !!value_name)) %>%
-    distinct()
+barplot_tf = function(df, selTF){
+
+  df %>%
+    rownames_to_column(var = "tf") %>%
+    dplyr::filter(tf == selTF)  %>% 
+    reshape2::melt() %>%
+    arrange(value) %>%
+    dplyr::mutate(variable = factor(variable, variable),
+           effect = factor(sign(value), c(-1,1))) %>%
+    ggplot(aes(x=variable, y=value, fill=effect)) +
+    geom_col() +
+    coord_flip() +
+    labs( x = "Sample/Contrast:", y = "Normalized Enrichment scores (NES)") +
+    theme_minimal() +
+    theme(legend.position = "none") +
+    scale_fill_manual(values = rwth_color(c("magenta", "green")),
+                      drop=F) +
+    theme(aspect.ratio = c(1)) +
+    ggtitle(paste0("TF: ", selTF))
   
-  meta_regulon_data = regulon %>%
-    select(-c(target, mor, likelihood)) %>%
-    distinct()
-  
-  emat = E %>%
-    select(!!gene_name, !!id_name, !!value_name) %>%
-    spread(!!id_name, !!value_name, fill=0) %>%
-    drop_na() %>%
-    data.frame(row.names = 1, stringsAsFactors = F, check.names = F)
-  
-  viper_regulon = regulon %>%
-    df2regulon(regulator_name = regulator_name)
-  
-  activity_scores = viper(eset = emat, regulon = viper_regulon, nes = T,
-                          method = 'none', minsize = 4, eset.filter = F,
-                          adaptive.size = F) %>%
-    data.frame(stringsAsFactors = F, check.names = F) %>%
-    rownames_to_column(var = regulator_name) %>%
-    gather(key=!!id_name, value="activity", -!!regulator_name) %>%
-    as_tibble() %>%
-    inner_join(., meta_data, by=id_name) %>%
-    inner_join(., meta_regulon_data, by = regulator_name)
-  
-  return(activity_scores)
 }
 
-df2regulon = function(df, regulator_name = "tf") {
-  regulon = df %>%
-    split(.[regulator_name]) %>%
-    map(function(dat) {
-      targets = setNames(dat$mor, dat$target)
-      likelihood = dat$likelihood
-      list(tfmode = targets, likelihood = likelihood)
-    })
-  return(regulon)
-}
-
-rwth_color = function(colors) {
-  if (!all(colors %in% rwth_colors_df$query)) {
-    wrong_queries = tibble(query = colors) %>%
-      anti_join(rwth_colors_df, by="query") %>%
-      pull(query)
-    warning(paste("The following queries are not available:",
-                  paste(wrong_queries, collapse = ", ")))
-  }
-  tibble(query = colors) %>%
-    inner_join(rwth_colors_df, by="query") %>%
-    pull(hex)
-}
 
 plot_lollipop = function(df, top_n_hits, var, var_label) {
   var = enquo(var)
@@ -178,123 +94,5 @@ plot_lollipop = function(df, top_n_hits, var, var_label) {
     scale_color_manual(values = rwth_color(c("magenta", "green"))) +
     theme(legend.position = "none") +
     theme(aspect.ratio = c(1)) + 
-    ggtitle(title)
-}
-
-plot_heatmap = function(df, var="tf") {
-  mat = df %>%
-    select(!!var, contrast, activity) %>% 
-    spread(contrast, activity) %>%
-    data.frame(row.names = var, check.names = F)
-  
-  if (ncol(mat) > 1) {
-    pheatmap(mat, show_rownames = F)
-  } else if (ncol(mat) == 1) {
-    pheatmap(mat, cluster_rows = F, cluster_cols = F, show_rownames = F)
-  }
-}
-
-plot_network = function(network, num_nodes, var="tf", var_label = "TF") {
-  var_of_interest = unique(network[[var]])
-  reg_var_of_interest = unique(network$regulation)
-  
-  feature_name = network %>% pull(!!var) %>% unique()
-  contrast_name = network %>% pull(contrast) %>% unique()
-  title = paste0("Contrast: ", contrast_name, ", ", var_label,": ", feature_name)
-  
-  sub_network = network %>% 
-    drop_na() %>%
-    filter(effect != "not regulated") %>%
-    arrange(-importance) %>%
-    mutate(n = row_number()) %>%
-    filter(n <= num_nodes) %>%
-    select(!!var, target, mor, effect, regulation)
-  
-  if (nrow(sub_network) > 0) {
-    nodes_df = sub_network %>%
-      distinct_(var, "target", "effect", "regulation") %>%
-      gather(class, name, -effect, -regulation) %>%
-      mutate(effect = case_when(class != var ~ effect,
-                                class == var ~ regulation)) %>%
-      mutate(effect = factor(effect, c("upregulated", "downregulated"))) %>%
-      distinct(class, name, effect) %>%
-      mutate(id = row_number())
-    
-    edges_df = sub_network %>%
-      select(!!var, target, mor) %>%
-      inner_join(rename(nodes_df, !!var:=name, from=id), by=var) %>%
-      select(-c(class,effect)) %>%
-      inner_join(rename(nodes_df, target=name, to=id), by="target") %>%
-      select(from, to, mor)
-    
-    g = tbl_graph(nodes = nodes_df, edges = edges_df) %>%
-      ggraph(layout = "nicely") + 
-      geom_edge_link(arrow = arrow(), aes(edge_colour=mor)) + 
-      geom_node_point(aes(color = effect, shape=class), size=10) +
-      geom_node_text(aes(label = name), vjust = 0.4) + 
-      theme_graph() +
-      scale_color_manual(values = rwth_color(c("green50", "bordeaux50")), drop=F) +
-      scale_edge_color_manual(values = rwth_color(c("bordeaux", "green")), drop=F) +
-      scale_shape_manual(values = c(16,15)) +
-      theme(legend.position = "none",
-            aspect.ratio = c(1), 
-            plot.title = element_text(size = 14, face="plain")) +
-      ggtitle(title)
-  } else {
-    nodes_df = tribble(
-      ~effect, ~class, ~name, ~id,
-      reg_var_of_interest, "tf", var_of_interest, 1
-    )
-    # nodes_df = sub_network %>%
-    #   distinct_(var, "target", "effect", "regulation") %>%
-    #   gather(class, name, -effect, -regulation) %>%
-    #   mutate(effect = case_when(class != var ~ effect,
-    #                             class == var ~ regulation)) %>%
-    #   mutate(effect = factor(effect, c("upregulated", "downregulated"))) %>%
-    #   distinct(class, name, effect) %>%
-    #   filter(class == "tf") %>%
-    #   mutate(id = row_number())
-    edges_df = NULL
-    
-    g = tbl_graph(nodes = nodes_df, edges = edges_df) %>%
-      ggraph(layout = "nicely") + 
-      geom_node_point(aes(color = effect, shape=class), size=10) +
-      geom_node_text(aes(label = name), vjust = 0.4) + 
-      theme_graph() +
-      scale_color_manual(values = rwth_color(c("green50", "red50")), drop=F) +
-      scale_edge_color_manual(values = rwth_color(c("red", "green")), drop=F) +
-      scale_shape_manual(values = c(15)) +
-      theme(legend.position = "none",
-            aspect.ratio = c(1)) +
-      ggtitle(title)
-  }
-  return(g)
-}
-
-plot_volcano = function(df, interactome, selected_top_n_labels, var, var_label = "TF") {
-  var = enquo(var)
-  
-  feature_name = interactome %>% pull(!!var) %>% unique()
-  contrast_name = df %>% pull(contrast) %>% unique()
-  
-  title = paste0("Contrast: ", contrast_name, ", ", var_label,": ", feature_name)
-  
-  interactome %>% 
-    inner_join(df, by=c("target")) %>%
-    mutate(effect = factor(effect, c("downregulated", "not regulated", "upregulated")),
-           regulated = case_when(effect %in% c("downregulated", "upregulated") ~ "yes",
-                                 effect %in% c("not regulated") ~ "no")) %>%
-    arrange(desc(regulated), -importance) %>%
-    mutate(n = row_number(),
-           label = case_when(n <= selected_top_n_labels & effect != "not regulated" ~ target,
-                             TRUE ~ "")) %>%
-    ggplot(aes(x=logFC, y=-log10(adj.p.value), label=label, color=effect)) +
-    geom_point() +
-    geom_label_repel() +
-    theme_minimal() +
-    theme(legend.position = "none") +
-    scale_color_manual(values = rwth_color(c("bordeaux", "black50","green")),
-                       drop=F) +
-    theme(aspect.ratio = c(1)) +
     ggtitle(title)
 }
