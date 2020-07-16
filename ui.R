8160098e4daff65a11e804fbab348b43a5583166
rosherbal <rosa.hernansaiz@bioquant.uni-heidelberg.de>
2020-07-16 14:41:50 +0200

    user interface for dorothea results visualization

--- ui.R
+++ ui.R
@@ -3,21 +3,60 @@ source("sub/global.R")
 ui = function(request) {
   fluidPage(
     useShinyjs(),
-    tags$head(includeScript("google-analytics.js")),
-    #shinyjs::addClass(id = "welcome", class = "navbar-right"),
-    #theme = shinythemes::shinytheme("spacelab"),
-    navbarPage(
-      id = "menu", title="FUNKI",collapsible=T,
-      footer = column(12, align="center", "FUNKI-App 2018 (version: 0.1)"),
-      source("sub/01_ui_welcome.R")$value,
-      source("sub/02_ui_upload.R")$value,
-      source("sub/03_ui_dorothea.R")$value
-      #,source("sub/04_ui_progeny.R")$value
-      #,source("sub/05_ui_kinact.R")$value
-      #,source("sub/06_ui_integration.R")$value
-      ,source("sub/ui_help.R")$value
-      ,source("sub/ui_contact.R")$value
-      ,hr()
-      ) # close navbarPage
-    ) # close fluidPage
+      title = "DoRothEA",
+    
+      # Column with title and logo
+      fluidRow(
+        column(
+          12, align="center", 
+          img(src="logo_dorothea.png", align = "right", height=75, width=75),
+        )),
+    
+    hr(),
+    
+    
+    fluidRow(
+      # select the column to render the plots
+      column(
+        6, align="center", uiOutput("dorothea_select_contrast")
+      ),
+      # select TF for specific plots
+      column(
+        6, align="center", uiOutput("select_tf")
+      )
+    ),
+    
+    hr(),
+    
+    fluidRow(
+      #select number of TFs to show in the graphics
+      column(
+        3, uiOutput("select_top_n_hits")
+      ),
+      column(
+        8, plotOutput("barplot_nes")
+      )
+    ),
+    
+    hr(),
+     fluidRow(
+    #   column(
+    #     4, plotOutput("dorothea_lollipop")
+    #   ),
+        column(
+          6, plotOutput("tf_bar")
+        ),
+       # column(
+       #   4, plotOutput("tf_network")
+       # )
+     ),
+    hr(),
+    
+    # Table visualization
+      DT::dataTableOutput("dorothea_result")
+      
+    
+
+  ) # close fluidPage
 }
+
