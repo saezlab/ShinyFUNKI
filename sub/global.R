9bfb86dd61b836ab94cad2b16db66297afa81487
rosherbal <rosa.hernansaiz@bioquant.uni-heidelberg.de>
2020-07-28 10:49:45 +0200

    plot function for network of TF

--- sub/global.R
+++ sub/global.R
@@ -2,21 +2,33 @@
 #initialize
 
 library(shiny)
+#library(useShinyjs)
 library(DT)
 library(tidyverse)
 library(dorothea)
 library(ggplot2)
 library(dplyr)
 library(reshape2)
+library(tidygraph)
+library(ggraph)
 
 # shiny options
 enableBookmarking(store = "server")
 options(shiny.maxRequestSize=30*1024^2)
 
 # Load data
-dorothea_result = read.csv( "data/examples/dorothea_scores_ABC.csv", row.names = 1 )
+
+# input for dorothea
+inputDorothea <- read.csv("data/examples/data_test.csv", row.names = 1 )
+
+# dorothea results
+d_file = "data/examples/dorothea_scores_ABC.csv"
+dorothea_result = read.csv( d_file, row.names = 1 ) # from VRE
 data(dorothea_hs, package = "dorothea")
 
+#get confidence levels
+confidence_level = unlist(strsplit(  gsub(".csv", "", d_file, fixed = T) , split="_" ))
+confidence_level = unlist(strsplit( confidence_level[length(confidence_level)] , split="" ))
 
 # PLOTS -------------------------------------------------------------
 
@@ -30,19 +42,16 @@ barplot_nes = function(df, smpl, nHits){
     
   title = paste("Sample/Contrast:", smpl, sep = " ")
   
-  ggplot(df, aes(x = reorder(GeneID, NES), y = NES)) +
+  ggplot(df, aes(x = NES, y = reorder(GeneID, NES) )) +
     geom_bar(aes(fill = NES), stat = "identity") +
     scale_fill_gradient2(low = "darkblue", high = "indianred",
                          mid = "whitesmoke", midpoint = 0) +
     theme_minimal() +
     theme(axis.title = element_text(face = "bold", size = 12),
-          axis.text.x =
-            element_text(angle = 45, hjust = 1, size =10, face= "bold"),
-          axis.text.y = element_text(size =10, face= "bold"),
-          panel.grid.major = element_blank(),
-          panel.grid.minor = element_blank()) +
-    xlab("Transcription Factors") +
-    ylab("Normalized Enrichment scores (NES)") +
+          axis.text.x = element_text(hjust = 1, size = 15, face= "bold"),
+          axis.text.y = element_text(size = 15, face= "bold")) +
+    ylab("Transcription Factors") +
+    xlab("Normalized Enrichment scores (NES)") +
     ggtitle(title)
 }
 
@@ -58,41 +67,37 @@ barplot_tf = function(df, selTF){
     ggplot(aes(x=variable, y=value, fill=effect)) +
     geom_col() +
     coord_flip() +
-    labs( x = "Sample/Contrast:", y = "Normalized Enrichment scores (NES)") +
+    labs( x = "Sample/Contrast", y = "Normalized Enrichment scores (NES)") +
     theme_minimal() +
-    theme(legend.position = "none") +
-    scale_fill_manual(values = rwth_color(c("magenta", "green")),
+    theme(legend.position = "none", 
+          axis.title = element_text(face = "bold", size = 12),
+          axis.text.x = element_text(hjust = 1, size = 15, face= "bold"),
+          axis.text.y = element_text(size = 15, face= "bold")) +
+    scale_fill_manual(values = c("#99004C", "#4C9900"),
                       drop=F) +
     theme(aspect.ratio = c(1)) +
     ggtitle(paste0("TF: ", selTF))
   
 }
 
-
-plot_lollipop = function(df, top_n_hits, var, var_label) {
-  var = enquo(var)
-  title = paste("Contrast:", unique(df$contrast))
-  df %>% 
-    arrange(activity) %>%
-    mutate(!!var := as_factor(!!var),
-           effect = factor(sign(activity)),
-           abs_activity = abs(activity)) %>%
-    group_by(effect) %>%
-    top_n(top_n_hits, abs_activity) %>%
-    ungroup() %>%
-    ggplot(aes(x=!!var, y=activity, color=effect)) +
-    geom_segment(aes(x=!!var, xend=!!var, y=0, yend=activity), color="grey") +
-    geom_point(size=4) +
-    coord_flip() +
-    theme_light() +
-    theme(
-      panel.grid.major.y = element_blank(),
-      panel.border = element_blank(),
-      axis.ticks.y = element_blank()
-    ) +
-    labs(x = var_label, y="Activity (z-score)") +
-    scale_color_manual(values = rwth_color(c("magenta", "green"))) +
-    theme(legend.position = "none") +
-    theme(aspect.ratio = c(1)) + 
+plot_network = function( network, nodes, title ){
+  
+  edges = edges %>% 
+    dplyr::filter(target %in% unique(nodes$target))
+  colnames(edges) = c("from", "sign", "to")
+  
+  
+  tbl_graph(nodes = nodes, edges = edges) %>%
+    ggraph(layout = "nicely") + 
+    geom_edge_link(arrow = arrow(), aes(edge_colour=as.factor(sign))) + 
+    geom_node_point(aes(color = regulation), size=10) + #, shape=class
+    geom_node_text(aes(label = target), vjust = 0.4) + 
+    theme_graph() +
+    scale_color_manual(values = c("#99004C", "#4C9900"), drop=F) +
+    scale_edge_color_manual(values = c("#99004C", "#4C9900"), drop=F) +
+    scale_shape_manual(values = c(16,15)) +
+    theme(legend.position = "none",
+          aspect.ratio = c(1), 
+          plot.title = element_text(size = 14, face="plain")) +
     ggtitle(title)
 }
