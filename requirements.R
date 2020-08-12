3bc08085c98f24b1f11ef8f550b7ad80f072339f
rosherbal <rosa.hernansaiz@bioquant.uni-heidelberg.de>
2020-08-12 17:27:42 +0200

    create list of dependencies

---
+++ requirements.R
@@ -0,0 +1,21 @@
+# First, install renv (if it is needed!)
+if (!requireNamespace("remotes"))
+  install.packages("remotes")
+
+remotes::install_github("rstudio/renv")
+
+renv::init()
+
+# Bioconductor itself must be recorded
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+
+# This line adding the Bioconductor repositories is needed,
+# in order to have the URLs to their repos in the
+# reproducible snapshot
+options(repos=structure(BiocManager::repositories()))
+
+BiocManager::install("progeny")
+
+# At last, the snapshot is generated
+renv::snapshot()
