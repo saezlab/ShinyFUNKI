d41a70593a8a882bbd42aa5a515f5f6494d87e21
rosherbal <rosa.hernansaiz@bioquant.uni-heidelberg.de>
2020-08-18 12:45:03 +0200

    before requirements.R

---
+++ bootstrap.R
@@ -0,0 +1,13 @@
+
+#.libPaths( c( "R/libs", .libPaths()) )
+
+options(renv.consent = TRUE)
+
+# Install renv (if it is needed!)
+if (!requireNamespace("renv", quietly = TRUE)) {
+	install.packages("renv")
+}
+
+renv::restore(packages=c('BiocManager'), prompt=FALSE)
+renv::restore(repos=BiocManager::repositories(), prompt=FALSE)
+renv::isolate()
