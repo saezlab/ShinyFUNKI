72a3609af7be9286625477b8b6e486893efee5da
rosherbal <rosa.hernansaiz@bioquant.uni-heidelberg.de>
2021-03-04 17:19:11 +0100

    updated

--- ui.R
+++ ui.R
@@ -11,8 +11,9 @@ ui = function(request) {
       footer = column(12, align="center", "FUNKI-App 2018 (version: 0.1)"),
       source("sub/01_ui_welcome.R")$value,
       source("sub/02_ui_upload.R")$value,
-      source("sub/03_ui_dorothea.R")$value
-      #,source("sub/04_ui_progeny.R")$value
+      source("sub/03_ui_dorothea.R")$value,
+      source("sub/04_ui_progeny.R")$value,
+      source("sub/07_ui_carnival.R")$value
       #,source("sub/05_ui_kinact.R")$value
       #,source("sub/06_ui_integration.R")$value
       ,source("sub/ui_help.R")$value
