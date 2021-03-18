de774ed37ab8d0b9c71931b15cbabd815006aa02
rosherbal <rosa.hernansaiz@bioquant.uni-heidelberg.de>
2021-03-18 14:55:04 +0100

    update footprint

--- ui.R
+++ ui.R
@@ -2,13 +2,12 @@
 source("sub/global.R")
 ui = function(request) {
   fluidPage(
+    
     useShinyjs(),
     tags$head(includeScript("google-analytics.js")),
-    #shinyjs::addClass(id = "welcome", class = "navbar-right"),
-    #theme = shinythemes::shinytheme("spacelab"),
     navbarPage(
       id = "menu", title="FUNKI",collapsible=T,
-      footer = column(12, align="center", "FUNKI-App 2018 (version: 0.1)"),
+      footer = column(12, align="center", "FUNKI-App 2021 (version: 0.1)"),
       source("sub/01_ui_welcome.R")$value,
       source("sub/02_ui_upload.R")$value,
       source("sub/03_ui_dorothea.R")$value,
