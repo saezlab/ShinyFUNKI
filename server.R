a0f0343d404b692b7f1fe23519b9b0b4572d3996
rosherbal <rosa.hernansaiz@bioquant.uni-heidelberg.de>
2021-05-06 16:49:40 +0200

    added kinact

--- server.R
+++ server.R
@@ -3,8 +3,7 @@ server = function(input, output, session) {
   source("sub/02_server_upload.R", local=T)
   source("sub/03_server_dorothea.R", local=T)
   source("sub/04_server_progeny.R", local=T)
-  source("sub/07_server_carnival.R", local=T)
   source("sub/05_server_kinact.R", local=T)
-  # source("sub/06_server_integration.R", local=T)
+  source("sub/07_server_carnival.R", local=T)
   # source("sub/server_bookmark.R", local=T)
 }
