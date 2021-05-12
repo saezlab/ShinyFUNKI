8f2c5a7f29fbde19af70f5c87f27898ef1dc4c74
rosherbal <rosa.hernansaiz@bioquant.uni-heidelberg.de>
2021-05-12 18:12:06 +0200

    updated renamed files

--- FUNKI/ui.R
+++ FUNKI/ui.R
@@ -12,8 +12,8 @@ ui = function(request) {
       source("sub/02_ui_upload.R")$value,
       source("sub/03_ui_dorothea.R")$value,
       source("sub/04_ui_progeny.R")$value,
-      source("sub/07_ui_carnival.R")$value,
-      source("sub/05_ui_kinact.R")$value
+      source("sub/05_ui_kinact.R")$value,
+      source("sub/06_ui_carnival.R")$value
       ,source("sub/ui_help.R")$value
       ,source("sub/ui_contact.R")$value
       ,hr()
