2b386b45edfa6b94900574d6bedae6fe99c958e9
rosherbal <rosa.hernansaiz@bioquant.uni-heidelberg.de>
2021-06-14 13:36:52 +0200

    added cosmos server

--- FUNKI/server.R
+++ FUNKI/server.R
@@ -5,4 +5,5 @@ server = function(input, output, session) {
   source("sub/04_server_progeny.R", local=T)
   source("sub/05_server_kinact.R", local=T)
   source("sub/06_server_carnival.R", local=T)
+  source("sub/07_server_cosmos.R", local=T)
 }
