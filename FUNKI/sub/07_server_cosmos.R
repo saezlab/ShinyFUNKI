1b784ee55fa235e782c66be8a2b04b3efcc1e59c
rosherbal <rosa.hernansaiz@bioquant.uni-heidelberg.de>
2021-08-11 16:03:25 +0200

    update UI and download structure

--- FUNKI/sub/07_server_cosmos.R
+++ FUNKI/sub/07_server_cosmos.R
@@ -4,7 +4,7 @@ cos = uploadResultsObjSever("upload_cosmos_results")
 # COSMOS
 COSMOS = reactive({
   
-  if(input$an_carnival){
+  if(input$an_cosmos){
     withProgress(message="Running COSMOS", value=1, {
       
       if(input$cosnet == "def"){
@@ -26,22 +26,28 @@ COSMOS = reactive({
       }
       
       # CARNIVAL parameters
-      if (input$solver == "lpSolve"){
-        solverpath = solverPath_cosmos
+      if (input$solver_cosmos == "lpSolve"){
+        solverpath_cosmos = NULL
       }
+
+      data = expr() %>%
+        tibble::rownames_to_column("HGNC") %>%
+        dplyr::select(!HGNC) %>%
+        unique.data.frame()
       
       cosmos <- run_COSMOS(layer_1 = layer_1, 
                            layer_2 = layer_2, 
-                           RNA_data = expr(), 
+                           RNA_data = data, 
                            PKN = PKN, 
                            solver = input$solver_cosmos, 
-                           solver_path = solverpath,
+                           solver_path = solverpath_cosmos,
                            runtime = c(200,200,1000,1000))
-      
     })
   }else{
     cosmos = cos()
   }
+  
+  return(cosmos)
 })
 
 #objects for visualisation
@@ -67,7 +73,7 @@ nodes_cosmos <- reactive({
   
   #nodes
   nodes <- COSMOS()[[2]] %>%
-    dplyr::filter(ZeroAct != 100) %>%
+    dplyr::filter(ZeroAct != 1) %>%
     dplyr::filter(Nodes %in% c(union(edges_cosmos()$from, edges_cosmos()$to))) %>%
     dplyr::rename(id = Nodes) %>%
     dplyr::mutate(label = id) %>%
@@ -75,7 +81,7 @@ nodes_cosmos <- reactive({
                                            NodeType == "S" ~ "diamond",
                                            TRUE ~ "circle")) %>%
     dplyr::mutate(title = paste0("<p><b>", label, "</b><br> (", AvgAct, ")</p>")) %>%
-    dplyr::mutate(color = findInterval(AvgAct, seq(from = -100, to = 100, length.out = 100))) %>%
+    dplyr::mutate(color = findInterval(AvgAct, seq(from = -1, to = 1, length.out = 50))) %>%
     dplyr::mutate(color = pal[color]) %>%
     dplyr::mutate(color = plotly::toRGB(color, alpha = 1))
   
@@ -86,18 +92,30 @@ nodes_cosmos <- reactive({
 output$select_node_cosmos = renderUI({
   req(COSMOS())
     
-    choices = COSMOS()[[2]] %>% #C()$nodesAttributes$Node %>%
-      dplyr::filter(ZeroAct != 100) %>%
+    choices = COSMOS()[[2]] %>%
+      dplyr::filter(ZeroAct != 1) %>%
       dplyr::select(Nodes) %>%
       dplyr::pull() %>%
       stringr::str_sort(numeric = T) %>%
       unique()
     
-    pickerInput(inputId = "focus_node",
+    pickerInput(inputId = "focus_node_cosmos",
                 label = "Focus on node:",
                 choices = choices,
                 options = list("live-search" = TRUE),
                 selected = NULL)
+})
+
+output$down_cosmos = renderUI({
+  req(COSMOS())
+  choices = list("COSMOS result (rds)" = 1, 
+                 "COSMOS network (csv)" = 2, 
+                 "COSMOS attributes (csv)" = 3)#,
+  # "Heatmap" = 5)
+  pickerInput(inputId = "down_cosmos",
+              label = "Select Download",
+              choices = choices,
+              selected = 1)
 })
 
 # Plots ---------------------------------------------------
@@ -142,24 +160,27 @@ observeEvent(input$hierarchical_cosmos,{
 
 # Download Handler --------------------------------------------------------
 
-output$download_cosmos = downloadHandler(
-  filename = "footprint_cosmos_saezLab.tar.gz",
-  content = function(x) {
-    fdir = "footprint_cosmos_saezLab"
-    
-    if (dir.exists(fdir)) {
-      do.call(file.remove, list(list.files(fdir, full.names = TRUE)))
-    } else{
-      dir.create(fdir)
-    }
-    
-    saveRDS(COSMOS(), file = paste0("cosmos_results_", input$select_sample_carnival, ".rds"))
-    write.csv(COSMOS()$weightedSIF,
-              file.path(fdir, paste0("cosmos_network_", input$select_sample_carnival, ".csv")),
-              quote = F)
-    write.csv(COSMOS()$nodesAttributes,
-              file.path(fdir, paste0("cosmos_nodesAttributes_", input$select_sample_carnival, ".csv")),
-              quote = F)
-    tar(x, files = fdir, compression = "gzip")
+cosmos_download = observeEvent({
+  input$down_cosmos
+},{
+  req(COSMOS())
+  
+  if(input$down_cosmos == 1){
+    a = list(fname = "COSMOS.rds",
+             cont = function(file){COSMOS() %>% saveRDS(., file = file)})
+  }else if(input$down_cosmos == 2){
+    a = list(fname = function(){paste0("COSMOS_network.csv")},
+             cont = function(file){write.csv(COSMOS()[[1]], file, quote = F)})
+  }else if(input$down_cosmos == 3){ 
+    a = list(fname = function(){paste0("COSMOS_nodesAttributes.csv")},
+             cont = function(file){write.csv(COSMOS()[[2]], file, quote = F)})
+  }else if(input$down_cosmos == 4){
+    a = list(fname = paste0("network_cosmos_targets_", input$select_tf, "_", input$select_contrast, ".png"),
+             cont = function(file){
+               visSave(network_tf_reactive(), "temp.html")
+               webshot::webshot("temp.html", zoom = 2, file = file)
+               file.remove("temp.html")})
   }
-)
+  
+  downloadObjSever("download_cosmos", filename = a$fname, content = a$cont)
+})
--- FUNKI/sub/07_ui_cosmos.R
+++ FUNKI/sub/07_ui_cosmos.R
@@ -9,25 +9,21 @@ tabPanel(
                        content = "Visualise the COSMOS results that you already have. The format should be a rds file.")
   ),
   # Network visualisation --------
-  
   fluidRow(
     h3("Network visualization"),
     hr(),
-    column(12, align="center", 
-           sidebarLayout(
-             sidebarPanel(
-               width = 2,
-               uiOutput("select_node_cosmos"),
-               checkboxInput("hierarchical_cosmos", label = "Hierarchical layout", value = FALSE),
-               downloadButton(
-                 outputId = "download_cosmos",
-                 label = "Download results"
-               )
-             ),
-             mainPanel(width = 10,
-                       visNetwork::visNetworkOutput("network_cosmos"))
-           )
+    shinyWidgets::dropdown(
+      inputId = "cosmos_control",
+      uiOutput("select_node_cosmos"),
+      checkboxInput("hierarchical", label = "Hierarchical layout", value = FALSE),
+      uiOutput("down_cosmos"),
+      downloadObjUI(id = "download_cosmos"),
+      circle = TRUE,
+      status = "primary",
+      icon = icon("sliders"), width = "300px"
     ),
-  ),
-  hr()
+    
+    visNetwork::visNetworkOutput("network_cosmos"),
+    
+  )
 )
